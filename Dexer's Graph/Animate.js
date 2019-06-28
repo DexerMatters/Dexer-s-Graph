683192ac4f427d4011efc31dcb924a94de19087c
DexerMatters <2353708378@qq.com>
2019-06-28 22:43:22 +0800

    更新一个类和未完工的动画列表

--- Dexer's Graph/Animate.js
+++ Dexer's Graph/Animate.js
@@ -1,4 +1,5 @@
 function dex_SimpleAnimation(){
+	dex_SimpleAnimation.AND="and";
 	var instance=this;
 	var orginalStyle;
 	var time=-1;
@@ -12,13 +13,19 @@ function dex_SimpleAnimation(){
 		lengths.push(length);
 		durings.push((begin===null?0:begin)+during);
 		froms.push(from);
-		begins.push(begin===null?0:begin);
+		if(begin=="and")
+			begins.push(dex_Checker.checkForLog(durings[begins.length-1]));
+		else begins.push(begin===null?0:begin);
 		rates.push(rate===null?new dex_Rate(1):rate);
 	};
 	var finish_=function(func){
 		t_enable=false;
 		time=-1;
 		instance.onFinish();
+		if(resetable) ele.style.cssText=orginalStyle;
+	}
+	var reset_=function(){
+		t_enable=false;
 		if(resetable) ele.style.cssText=orginalStyle;
 	}
 	this.onFinish=function(func){
@@ -83,7 +90,7 @@ function dex_SimpleAnimation(){
 				if(time<durings[i]&&time>=begins[i]){
 					for(var j=0;j<rates[i].getCount();j++)
 						if(time>durings[i]*(j/rates[i].getCount())&&time-begins[i]<=durings[i]*((j+1)/rates[i].getCount())){
-							console.log(rates[i].getTranslatedValues()[j]);
+							//console.log(rates[i].getTranslatedValues()[j]);
 							switch(styles[i]){
 								case "opacity":
 									eval("ele.style."+styles[i]+"=(parseFloat(ele.style."+styles[i]+")+"+rates[i].getTranslatedValues()[j]*(lengths[i]/durings[i])+");");
@@ -136,15 +143,30 @@ function dex_SimpleAnimation(){
 	this.setReducible=function(bool){
 		resetable=bool;
 	};
+	this.getDuration=function(i){
+		return durings[i]
+	}
 	this.startAt=function(id){
 		t_enable=true;
 		if(time!=-1) return;
 		ele=document.getElementById(id);
 		orginalStyle=ele.style.cssText;
-		for (var i = 0; i < styles.length; i++){
-			
-		}
 		loop();
+	};
+}
+function dex_AnimationList(anis){
+	var ele;
+	var list=anis;
+	this.startAt=function(id){
+		for (var i=0;i<list.length-1;i++){
+			list[i].onFinish(function()
+				list[i+1].startAt(id);
+			});
+		}
+		list[0].startAt(id);
+	};
+	this.add=function(ani){
+		list.push(ani);
 	};
 }
 function dex_Effect(id){
@@ -154,8 +176,7 @@ function dex_Effect(id){
 		var during_=during===null?200:during;
 		//an.addFade(1,-1,50,0,null)
 		//an.addFade(0,2,50,50,null)
-		an.addMoveX(0,400,1000,0,new dex_Rate(1,2,3))
-		an.setReducible(false);
-		an.startAt(id);
+		an.addMoveX(0,400,500,0,new dex_Rate(1))
+		
 	}
 }
